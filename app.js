8848d8a78dc6e36af9820dbc15ea76c23036eb29
jalagor <johnrossrogala@gmail.com>
2024-06-03 19:40:13 -0600

    replaced player card h2 with h3, adjusted h3 font-size

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const diceUrl = {
-    one: "https://cdn.pixabay.com/photo/2014/04/03/10/24/one-310338_960_720.png",
-    two: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312627_960_720.png",
-    three:"https://cdn.pixabay.com/photo/2014/04/03/10/24/three-310336_960_720.png",
-    four: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312623_960_720.png",
-    five: "https://cdn.pixabay.com/photo/2014/04/03/10/24/five-310334_960_720.png",
-    six: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312621_960_720.png" 
-}
+  one: "https://cdn.pixabay.com/photo/2014/04/03/10/24/one-310338_960_720.png",
+  two: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312627_960_720.png",
+  three:
+    "https://cdn.pixabay.com/photo/2014/04/03/10/24/three-310336_960_720.png",
+  four: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312623_960_720.png",
+  five: "https://cdn.pixabay.com/photo/2014/04/03/10/24/five-310334_960_720.png",
+  six: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312621_960_720.png",
+};
