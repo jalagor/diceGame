894383222cbbb8fdc4cff0f7949db23f1a48d7b2
jalagor <johnrossrogala@gmail.com>
2024-06-03 20:07:47 -0600

    created randomNumber function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const diceUrl = {
-  one: "https://cdn.pixabay.com/photo/2014/04/03/10/24/one-310338_960_720.png",
-  two: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312627_960_720.png",
-  three:
-    "https://cdn.pixabay.com/photo/2014/04/03/10/24/three-310336_960_720.png",
-  four: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312623_960_720.png",
-  five: "https://cdn.pixabay.com/photo/2014/04/03/10/24/five-310334_960_720.png",
-  six: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312621_960_720.png",
-};
+    1: "https://cdn.pixabay.com/photo/2014/04/03/10/24/one-310338_960_720.png",
+    2: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312627_960_720.png",
+    3: "https://cdn.pixabay.com/photo/2014/04/03/10/24/three-310336_960_720.png",
+    4: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312623_960_720.png",
+    5: "https://cdn.pixabay.com/photo/2014/04/03/10/24/five-310334_960_720.png",
+    6: "https://cdn.pixabay.com/photo/2014/04/03/11/56/dice-312621_960_720.png",
+  },
+  randomNumber = () => {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    return diceUrl[roll];
+  };
