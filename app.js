f1dc441ba26fc03330b7a30c7127e286bf8ab332
jalagor <johnrossrogala@gmail.com>
2024-06-03 20:52:08 -0600

    added playGame function

--- app.js
+++ app.js
@@ -8,5 +8,24 @@ const diceUrl = {
   },
   randomNumber = () => {
     let roll = Math.floor(Math.random() * 6) + 1;
-    return diceUrl[roll];
-  };
+    return roll;
+  }, 
+  playGame = () => {
+    p1 = randomNumber(),
+    p2 = randomNumber();
+
+    player1Di.setAttribute('src', diceUrl[p1])
+    player2Di.setAttribute('src', diceUrl[p2])
+    
+    if (p1 > p2){
+        header.innerText = "Player 1 Wins!"
+    } else if (p1 < p2){
+        header.innerText = "Player 2 Wins!"
+    } else {
+        header.innerText = "Tie! Roll Again!"
+    }
+  }
+  player1Di = document.querySelector("#player1Di"),
+  player2Di = document.querySelector("#player2Di"),
+  header = document.querySelector('h1');
+  
